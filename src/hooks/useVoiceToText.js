f934f940421653a718dde0c79fa56340e8b1635a
Amin Partovi <amin.frontend@gmail.com>
2023-12-10 13:02:50 +0330

    fix: stop functionality

--- src/hooks/useVoiceToText.js
+++ src/hooks/useVoiceToText.js
@@ -1,28 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const useVoiceToText = () => {
   const [text, setText] = useState("");
+  const continuous = useRef(true);
+
   let recognition = new SpeechRecognition();
-  let subscribers = [];
 
   function start() {
     recognition.start();
-    subscribers.push(start);
   }
 
   function stop() {
     recognition.stop();
-    subscribers = [];
+    continuous.current = false;
   }
 
   recognition.onend = () => {
-    if (subscribers.length > 0) {
-      subscribers[0]();
-    } else {
-      console.log("Nothing to execute.");
+    if (continuous.current) {
+      start();
     }
   };
 
